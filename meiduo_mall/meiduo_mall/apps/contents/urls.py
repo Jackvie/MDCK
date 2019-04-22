66946a62b5e69b58c8931ca24b4c4d9c32026eed
Jackvie <15938721922@163.com>
2019-04-22 12:32:59 +0800

    m

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from contents import views
-
-urlpatterns = [
-            url(r"^index/$", views.IndexView.as_view(), name='index'),
-        ]
--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from users import views
-
-urlpatterns = [
-            url('^register/$', views.RegisterView.as_view(), name="register"),
-            url(r"^usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$", views.UsernameCountView.as_view()),
-        ]
